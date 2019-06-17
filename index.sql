87f18434b0fee286e245812da739e5c84a65208a
Shane Kaestner <shane.kaestner@yahoo.com>
2019-06-16 18:50:58 -0700

    added inventory

--- index.sql
+++ index.sql
@@ -1,5 +1,19 @@
-CREATE TABLE candle_shop (id INTEGER PRIMARY KEY, name TEXT, scent TEXT, size TEXT, price INTEGER);
+CREATE TABLE candle_shop (id INTEGER PRIMARY KEY, name TEXT, scent TEXT, size TEXT, price INTEGER, duration TEXT);
 
-INSERT INTO candle_shop VALUES(001, 'Misty Wave', 'Coconut-lilac','6 oz', 8);
-INSERT INTO candle_shop VALUES(002, 'Misty Wave', 'Coconut-lilac','12 oz', 12);
-INSERT INTO candle_shop VALUES(003, 'Misty Wave', 'Coconut-lilac','18 oz', 15);
+INSERT INTO candle_shop VALUES(001, 'Misty Wave', 'Coconut-Lilac','6 oz', 8, 'permanant');
+INSERT INTO candle_shop VALUES(002, 'Misty Wave', 'Coconut-Lilac','12 oz', 12, 'permanant');
+INSERT INTO candle_shop VALUES(003, 'Misty Wave', 'Coconut-Lilac','18 oz', 15, 'permanant');
+INSERT INTO candle_shop VALUES(004, 'English Pipe', 'Bergamot-Tobacco','6 oz', 9, 'permanant');
+INSERT INTO candle_shop VALUES(005, 'English Pipe', 'Bergamot-Tobacco','12 oz', 13, 'permanant');
+INSERT INTO candle_shop VALUES(006, 'English Pipe', 'Bergamot-Tobacco','18 oz', 16, 'permanant');
+INSERT INTO candle_shop VALUES(007, 'Dangerous Division', 'Tobacco-Coffee','6 oz', 10, 'permanant');
+INSERT INTO candle_shop VALUES(008, 'Dangerous Division', 'Tobacco-Coffee','12 oz', 15, 'permanant');
+INSERT INTO candle_shop VALUES(009, 'Dangerous Division', 'Tobacco-Coffee','18 oz', 17, 'permanant');
+INSERT INTO candle_shop VALUES(010, 'Corporal Funnel', 'Vanilla-Leather','6 oz', 15, 'gaining ground');
+INSERT INTO candle_shop VALUES(011, 'Corporal Funnel', 'Vanilla-Leather','12 oz', 15, 'gaining ground');
+INSERT INTO candle_shop VALUES(012, 'Scarlet Monastary', 'Rose-Leather','6 oz', 8, 'trial');
+INSERT INTO candle_shop VALUES(013, "Plain N' Clear", 'Unscented','6 oz', 4, 'trial');
+INSERT INTO candle_shop VALUES(014, 'Caught a light Sneeze', 'Pepper-Apple','6 oz', 7, 'trial');
+INSERT INTO candle_shop VALUES(015, "Dreamed a Little Dream",
+'Lilac-Rose-Vanilla','18 oz', 9, 'trial');
+ 
