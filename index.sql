e595fed965b22d8dffcaff66387a3542cf9fb110
Shane Kaestner <shane.kaestner@yahoo.com>
2019-06-16 18:02:33 -0700

    Initial Commit

---
+++ index.sql
@@ -0,0 +1,5 @@
+CREATE TABLE candle_shop (id INTEGER PRIMARY KEY, name TEXT, scent TEXT, size TEXT, price INTEGER);
+
+INSERT INTO candle_shop VALUES(001, 'Misty Wave', 'Coconut-lilac','6 oz', 8);
+INSERT INTO candle_shop VALUES(002, 'Misty Wave', 'Coconut-lilac','12 oz', 12);
+INSERT INTO candle_shop VALUES(003, 'Misty Wave', 'Coconut-lilac','18 oz', 15);
